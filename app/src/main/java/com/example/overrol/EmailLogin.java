12cda713191d079c778bafac0d6dfed3c00a9655
RalonsoC01 <72815754+RalonsoC01@users.noreply.github.com>
2020-11-20 12:25:44 +0100

    Prubas varias sobre el FireBase

--- app/src/main/java/com/example/overrol/EmailLogin.java
+++ app/src/main/java/com/example/overrol/EmailLogin.java
@@ -1,21 +1,106 @@
 package com.example.overrol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class EmailLogin extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class EmailLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText TextEmail;
+    private EditText TextPassword;
+    private Button btnRegistrarse;
+    private ProgressDialog progressDialog;
+
+
+    //Declaramos objeto Firebase
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_login);
+
+        //inicializamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Referenciamos los views
+        TextEmail = (EditText) findViewById(R.id.txtEmailEmailLogin);
+        TextPassword = (EditText) findViewById(R.id.txtContrasenaEmailLogin);
+
+        btnRegistrarse = (Button) findViewById(R.id.btnEmailAutenticar);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        btnRegistrarse.setOnClickListener(this);
     }
     public void RegresarLogin(View view)
     {
         Intent i= new Intent(this,Login.class);
+        startActivity(i);
+    }
+    private void IniciarUsuario() {
+
+        //Obtenemos el email y la contraseña desde las cajas de texto
+        String email = TextEmail.getText().toString().trim();
+        String password = TextPassword.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Se debe ingresar un email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressDialog.setMessage("Realizando registro en linea...");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(EmailLogin.this, "Se ha registrado el usuario con el email: " + TextEmail.getText(), Toast.LENGTH_LONG).show();
+                        } else {
+
+                            Toast.makeText(EmailLogin.this, "No se pudo registrar el usuario ", Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        IniciarUsuario();
+    }
+    public void acceder(View view)
+    {
+        Intent i= new Intent(this,inicio.class);
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/overrol/Login.java
+++ app/src/main/java/com/example/overrol/Login.java
@@ -1,21 +1,29 @@
 package com.example.overrol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
     public void acceder(View view)
     {
@@ -31,5 +39,11 @@ public class Login extends AppCompatActivity {
     {
         Intent i= new Intent(this,EmailLogin.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
