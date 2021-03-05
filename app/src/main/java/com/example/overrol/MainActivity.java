0bd45839c95b0e25ce098e03269d460a34295ea2
RalonsoC01 <72815754+RalonsoC01@users.noreply.github.com>
2021-03-05 17:59:49 +0100

    Animación de SplashScreen completa

--- app/src/main/java/com/example/overrol/MainActivity.java
+++ app/src/main/java/com/example/overrol/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -15,15 +16,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Pantalla completa
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Añadir las animaciones
         Animation animacion1= AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
         Animation animacion2= AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
 
-        TextView txtBienvenida = findViewById(txtBienvenida);
-        ImageView logo = findViewById(logo);
+        TextView Bienvenida = findViewById(R.id.txtBienvenida);
+        ImageView logo = findViewById(R.id.imglogo);
 
-        txtBienvenida.setAnimation(animacion1);
-        logo.setAnimation(animacion2);
+        Bienvenida.setAnimation(animacion2);
+        logo.setAnimation(animacion1);
 
         new Handler() .postDelayed (new Runnable() {
             @Override
